e918120645e8ab7a87bfff34a1c9917f5c72e583
Jim Safley <jimsafley@gmail.com>
2009-11-09 21:02:11 +0000

    Adds Zend_Form funtionality to ZoteroImport plugin.

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -6,15 +6,48 @@ class ZoteroImport_IndexController extends Omeka_Controller_Action
         $this->view->assign('form', $this->_getFeedForm());
     }
     
-    public function importAction()
+    public function importLibraryAction()
     {
         $form = $this->_getFeedForm();
+        if (!$form->isValid($_POST)) {
+            $this->view->assign('form', $form);
+            return $this->render('index');
+        }
     }
     
     protected function _getFeedForm()
     {
         require_once 'Zend/Form.php';
         $form = new Zend_Form;
+        $form->setAction($this->_helper->url('import-library', 'index', 'zotero-import'))
+             ->setMethod('post')
+             ->addElementPrefixPath('ZoteroApiClient_Validate', 'ZoteroApiClient/Validate/', 'validate')
+             ->removeDecorator('HtmlTag');
+        
+        $form->addElement('text', 'feed', array(
+            'label' => 'Zotero Atom Feed URL', 
+            'description' => 'Enter the Atom Feed URL of the Zotero library you want to import.', 
+            'class' => 'textinput', 
+            'required' => true, 
+            'validators' => array('zoteroapiurl'),
+            'decorators' => array(
+                'ViewHelper', 
+                'Errors', 
+                array('Description', array('tag' => 'p', 'class' => 'explanation')), 
+                array(array('InputsTag' => 'HtmlTag'), array('tag' => 'div', 'class' => 'inputs')), 
+                'Label', 
+                array(array('FieldTag' => 'HtmlTag'), array('tag' => 'div', 'class' => 'field'))
+            )
+        ));
+        
+        $form->addElement('submit', 'submit', array(
+            'label' => 'Continue', 
+            'class' => 'submit', 
+            'decorators' => array(
+                'ViewHelper'
+            )
+        ));
+        
         return $form;
     }
 }
---
+++ views/admin/index/import-library.php
@@ -0,0 +1,8 @@
+<?php
+$head = array('title' => html_escape('Zotero Import | Import Library'));
+head($head);
+?>
+<h1><?php echo $head['title']; ?></h1>
+<div id="primary">
+</div>
+<?php foot(); ?>
