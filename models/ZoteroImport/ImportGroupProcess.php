4a50ba3adf426e2030379c8647636c6200ed1926
Jim Safley <jimsafley@gmail.com>
2009-11-25 03:55:14 +0000

    Adds feed iteration and removes unneeded code.

--- models/ZoteroImport/ImportGroupProcess.php
+++ models/ZoteroImport/ImportGroupProcess.php
@@ -3,24 +3,45 @@
 
 class ZoteroImport_ImportGroupProcess extends ProcessAbstract
 {
-    protected $_feed = array();
-    protected $_entries = array();
+    protected $_feed;
+    protected $_entries;
+    protected $_params = array('content' => 'full', 'start' => 0);
     
     public function run($args)
     {
-        // /usr/bin/php /var/www/omekatag/application/core/background.php -p 30 -l initializeRoutes
-        // getting a 500 Internal Server Error
+        /*
         require_once 'ZoteroApiClient/Service/Zotero.php';
         $z = new ZoteroApiClient_Service_Zotero($args['username'], $args['password']);
         $location = $z->userItemFile(66453, 75201954);
         exit($location);
+        */
         
 /******************************************************************************/
         
         // /usr/bin/php /var/www/omekatag/application/core/background.php -p 29 -l initializeRoutes
         require_once 'ZoteroApiClient/Service/Zotero.php';
         $z = new ZoteroApiClient_Service_Zotero;
-        $feed = $z->groupItemsTop($args['id'], array('content' => 'full'));
+        
+        do {
+            $feed = $z->groupItemsTop($args['id'], array('content' => 'full', 
+                                                         'start'   => $this->_params['start']));
+            
+            // Set the feed and entries more useable formats.
+            $this->_setFeed($feed);
+            
+            // Map Zotero to Omeka.
+            //$this->_map();
+            
+            echo $this->_feed['link']['self']."\n";
+            
+        } while ($this->_feed['link']['self'] != $this->_feed['link']['last']);
+    }
+    
+    protected function _setFeed(Zend_Feed_Atom $feed)
+    {
+        // Reset the feed and entries properties.
+        $this->_feed = array();
+        $this->_entries = array();
         
         // Set Zotero feed variables.
         $this->_feed['title']        = (string) $feed->title;
@@ -48,6 +69,13 @@ class ZoteroImport_ImportGroupProcess extends ProcessAbstract
                 default:
                     break;
             }
+        }
+        
+        // Set the start parameter for the next page iteration.
+        if ($this->_feed['link']['next']) {
+            $query = parse_url($this->_feed['link']['next'], PHP_URL_QUERY);
+            parse_str($query, $query);
+            $this->_params['start'] = $query['start'];
         }
         
         // Map Zotero entry to Omeka item.
@@ -115,91 +143,5 @@ class ZoteroImport_ImportGroupProcess extends ProcessAbstract
                 $this->_entries[$itemID]['item']['creators'][$creator['key']]['name'] = (string) $creator->creator->name;
             }
         }
-        
-        
-        print_r($this->_entries);
-        /*
-        $z = new ZoteroImport_Service_Zotero();
-        
-        //$result = $z->userItemsTop(66453, array('content' => 'full'));
-        //$result = $z->groupItems(3113, array('start' => 300, 'content' => 'full'));
-        //print_r($result);
-        
-        $groupId = 3113;
-        
-        do {
-            
-            // Start iteration at 0.
-            if (!isset($start)) {
-                $start = 0;
-            }
-            
-            // Get the request response.
-            $xml = $z->groupItems($groupId, array('start' => $start, 'content' => 'full'))->getIterator();
-            
-            // Register XPath namespaces.
-            $xml->registerXPathNamespace('atom', 'http://www.w3.org/2005/Atom');
-            $xml->registerXPathNamespace('zapi', 'http://zotero.org/ns/api');
-            $xml->registerXPathNamespace('zxfer', 'http://zotero.org/ns/transfer');
-            
-            // Set the self, next, and last page URLs.
-            $links = array('self', 'next', 'last');
-            foreach ($links as $link) {
-                $$link = $xml->xpath("atom:link[@rel='$link']/attribute::href");
-                $$link = (string) ${$link}[0];
-            }
-            
-            // Set the start parameter for the next page iteration.
-            if ($next) {
-                $query = parse_url($next, PHP_URL_QUERY);
-                parse_str($query);
-            }
-            
-            //echo "self: $self\nnext: $next\nlast: $last\n";
-            
-            // MAP ZOTERO ENTRY TO OMEKA ITEM...
-            $entries = $xml->entry;
-            foreach ($entries as $entry) {
-                
-                //print_r($entry);exit;
-                
-                $title      = (string) $entry->title;
-                $authorName = (string) $entry->name;
-                $authorUri  = (string) $entry->uri;
-                $id         = (string) $entry->id;
-                $published  = (string) $entry->published;
-                $updated    = (string) $entry->updated;
-                
-                $links = $entry->link;
-                foreach ($links as $link) {
-                    // get links
-                }
-                
-                $zapi = $entry->children('http://zotero.org/ns/api');
-                $itemId   = $zapi->itemId;
-                $itemType = $zapi->itemType;
-                $numTags  = $zapi->numTags;
-                
-                $content = $entry->content;
-                    
-                    $item = $content->item;
-                        
-                        $fields = $item->field;
-                        foreach ($fields as $field) {
-                            // get fields
-                        }
-                        
-                        $creators = $item->creator;
-                        foreach ($creators as $creator) {
-                            // get creators
-                        }
-            }
-            
-            //$totalResults = $xml->xpath('zapi:totalResults');
-            //print_r($totalResults);exit;
-        
-        // Stop iteration if the current and last pages are identical.
-        } while ($self != $last); // !$next ?
-        */
     }
 }
