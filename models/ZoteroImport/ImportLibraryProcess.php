1ed2f2a3e145bb2b0db300b5511b0fcfd36101e6
Jim Safley <jimsafley@gmail.com>
2009-12-08 19:13:19 +0000

    Fixes #53, Adds a Zotero element set containing all Zotero fields. Adds a comprehensive Zotero to Omeka mapping array, including the creator fields. Changes the way Zotero fields are mapped to Omeka elements: iterates the new mapping array instead of using a switch statement. Maps Zotero item notes to Omeka:Zotero:Note. Correctly maps the creator fields to the corresponding Omeka Zotero elements. Adds some descriptive comments.

--- models/ZoteroImport/ImportLibraryProcess.php
+++ models/ZoteroImport/ImportLibraryProcess.php
@@ -24,12 +24,12 @@ class ZoteroImport_ImportLibraryProcess extends ProcessAbstract
     {
         do {
             
-            // Initialize the start parameter on the first group feed iteration.
+            // Initialize the start parameter on the first library feed iteration.
             if (!isset($start)) {
                 $start = 0;
             }
             
-            // Get the group feed.
+            // Get the library feed.
             $method = "{$this->_libraryType}ItemsTop";
             $feed = $this->_client->$method($this->_libraryId, array('start' => $start));
             
@@ -44,7 +44,8 @@ class ZoteroImport_ImportLibraryProcess extends ProcessAbstract
             foreach ($feed->entry as $item) {
                 
                 // Set default insert_item() arguments.
-                $itemMetadata = array('collection_id' => $this->_collectionId);
+                $itemMetadata = array('collection_id' => $this->_collectionId, 
+                                      'public'        => true);
                 $elementTexts = array();
                 $fileMetadata = array('file_transfer_type'  => 'Url', 
                                       'file_ingest_options' => array('ignore_invalid_files' => true));
@@ -52,10 +53,29 @@ class ZoteroImport_ImportLibraryProcess extends ProcessAbstract
                 // Map the title.
                 $elementTexts['Dublin Core']['Title'][] = array('text' => $item->title(), 'html' => false);
                 
-                // Map Zotero fields to Omeka element texts (Dublin Core)
+                // Map the Zotero API field nodes to Omeka elements.
                 foreach ($item->content->div->table->tr as $tr) {
+                    
+                    // Only map those field nodes that exist in the mapping 
+                    // array.
                     if ($elementName = $this->_getElementName($tr['class'])) {
-                        $elementTexts['Dublin Core'][$elementName][] = array('text' => $tr->td(), 'html' => false);
+                        
+                        // Map the field nodes to the correlating Dublin Core
+                        // element set field elements.
+                        $elementTexts['Dublin Core'][$elementName['dc']][] = array('text' => $tr->td(), 'html' => false);
+                        
+                        // The creator node is formatted differently than other 
+                        // field nodes. Account for this by mapping a creator 
+                        // node to the correlating Zotero element set creator 
+                        // element.
+                        if ('creator' == $tr['class'] && in_array($tr->th(), $elementName['z'])) {
+                            $elementTexts['Zotero'][$tr->th()][] = array('text' => $tr->td(), 'html' => false);
+                        
+                        // Map the field nodes to the correlating Zotero element 
+                        // set field elements.
+                        } else {
+                            $elementTexts['Zotero'][$elementName['z']][] = array('text' => $tr->td(), 'html' => false);
+                        }
                     }
                 }
                 
@@ -65,7 +85,8 @@ class ZoteroImport_ImportLibraryProcess extends ProcessAbstract
                     $tags = $this->_client->$method($this->_libraryId, $item->itemID());
                     $tagArray = array();
                     foreach ($tags->entry as $tag) {
-                        // Remove commas from Zotero tag, or Omeka will bisect it.
+                        // Remove commas from Zotero tags, or Omeka will assume 
+                        // they are separate tags.
                         $tagArray[] = str_replace(',', ' ', $tag->title);
                     }
                     $itemMetadata['tags'] = join(',', $tagArray);
@@ -79,8 +100,8 @@ class ZoteroImport_ImportLibraryProcess extends ProcessAbstract
                         switch ($child->itemType()) {
                             case 'note':
                                 $noteXpath = '//default:tr[@class="note"]/default:td/default:p';
-                                $note = (string) $this->_contentXpath($child->content, $noteXpath, true);
-                                // Map note to what?
+                                $note = $this->_contentXpath($child->content, $noteXpath, true);
+                                $elementTexts['Zotero']['Note'][] = array('text' => (string) $note, 'html' => false);
                                 break;
                             case 'attachment':
                                 $urlXpath = '//default:tr[@class="url"]/default:td';
@@ -109,165 +130,12 @@ class ZoteroImport_ImportLibraryProcess extends ProcessAbstract
     
     protected function _getElementName($fieldName)
     {
-        // Map to Dublin Core.
-        switch ($fieldName) {
-            
-            // Source
-            case 'reporterVolume':
-            case 'firstPage': // ???
-            case 'codeVolume':
-            case 'codePages': // ???
-            case 'pages': // ???
-            case 'series':
-            case 'seriesNumber': // concatenate with series?
-            case 'volume':
-            case 'numberOfVolumes': // concatenate with volume?
-            case 'edition':
-            case 'issue':
-            case 'section':
-            case 'dictionaryTitle':
-            case 'encyclopediaTitle':
-            case 'proceedingsTitle':
-            case 'conferenceName':
-            case 'meetingName':
-            case 'seriesTitle':
-            case 'bookTitle':
-            case 'websiteTitle':
-            case 'publicationTitle':
-            case 'forumTitle':
-            case 'blogTitle':
-            case 'episodeNumber':
-               $elementName = 'Source';
-                break;
-            
-            // Identifier
-            case 'url':
-            case 'ISBN':
-            case 'ISSN':
-            case 'callNumber':
-            case 'DOI':
-            case 'reportNumber':
-            case 'billNumber':
-            case 'docketNumber':
-            case 'documentNumber':
-            case 'publicLawNumber':
-            case 'codeNumber':
-            case 'patentNumber':
-            case 'applicationNumber':
-            case 'priorityNumbers':
-            case 'code':
-                $elementName = 'Identifier';
-                break;
-            
-            // Publisher
-            case 'publisher':
-            case 'place': // concatenate with publisher?
-            case 'repository':
-            case 'archiveLocation':
-            case 'system':
-            case 'distributor':
-            case 'network':
-            case 'university':
-            case 'institution':
-            case 'journalAbbreviation':
-            case 'studio':
-            case 'label':
-                 $elementName = 'Publisher';
-                break;
-            
-            // Type
-            case 'itemType':
-            case 'thesisType':
-            case 'letterType':
-            case 'manuscriptType':
-            case 'videoRecordingType':
-            case 'websiteType':
-            case 'reportType':
-            case 'audioRecordingType':
-            case 'presentationType':
-            case 'postType':
-            case 'mapType':
-            case 'audioFileType':
-                $elementName = 'Type';
-                break;
-            
-            // Format
-            case 'programmingLanguage':
-            case 'interviewMedium':
-            case 'numPages':
-            case 'runningTime':
-            case 'artworkMedium':
-            case 'artworkSize':
-            case 'scale':
-                $elementName = 'Format';
-                break;
-            
-            // Creator
-            case 'creator':
-            case 'company':
-            case 'legislativeBody':
-            case 'court':
-            case 'committee':
-            case 'session':
-                $elementName = 'Creator';
-                break;
-            
-            // Date
-            case 'date':
-            case 'accessDate':
-            case 'dateDecided':
-            case 'dateEnacted':
-            case 'issueDate':
-                $elementName = 'Date';
-                break;
-            
-            // Description
-            case 'extra':
-            case 'abstractNote':
-            case 'seriesText':
-            case 'history':
-            case 'legalStatus':
-                $elementName = 'Description';
-                break;
-            
-            // Title
-            case 'title':
-            case 'shortTitle':
-            case 'subject': // email subject
-            case 'caseName':
-            case 'nameOfAct':
-                $elementName = 'Title';
-                break;
-            
-            // Contributor
-            case 'reporter':
-            case 'assignee':
-                $elementName = 'Contributor';
-                break;
-            
-            // Relation
-            case 'version':
-            case 'references':
-                $elementName = 'Relation';
-                break;
-            
-            // Rights
-            case 'rights':
-                $elementName = 'Rights';
-                break;
-            
-            // Language
-            case 'language':
-                $elementName = 'Language';
-                break;
-            
-            // [unknown]
-            default:
-                $elementName = false;
-                break;
+        foreach (ZoteroImportPlugin::$zoteroFields as $zoteroFieldName => $map) {
+            if ($fieldName == $zoteroFieldName) {
+                return $map;
+            }
         }
-        
-        return $elementName;
+        return false;
     }
     
     protected function _contentXpath(Zend_Feed_Element $content, $xpath, $fetchOne = false)
--- plugin.php
+++ plugin.php
@@ -6,8 +6,156 @@ add_filter('admin_navigation_main', 'ZoteroImportPlugin::adminNavigationMain');
 
 class ZoteroImportPlugin
 {
+    public static $zoteroFields = array(
+        'creator' => array('dc' => 'Creator',     'z' => array(
+            'Author', 
+            'Contributor', 
+            'Editor', 
+            'Translator', 
+            'Series Editor', 
+            'Interviewee', 
+            'Interviewer', 
+            'Director', 
+            'Scriptwriter', 
+            'Producer', 
+            'Cast Member', 
+            'Sponsor', 
+            'Counsel', 
+            'Inventor', 
+            'Attorney Agent', 
+            'Recipient', 
+            'Performer', 
+            'Composer', 
+            'Words By', 
+            'Cartographer', 
+            'Programmer', 
+            'Artist', 
+            'Commenter', 
+            'Presenter', 
+            'Guest', 
+            'Podcaster', 
+            'Reviewed Author', 
+        )), 
+        //'country' => array('dc' => '', 'z' => 'Country'), // ambiguous
+        //'number' => array('dc' => '', 'z' => 'Number'), // ambiguous
+        'DOI'                 => array('dc' => 'Identifier',  'z' => 'DOI'), 
+        'ISBN'                => array('dc' => 'Identifier',  'z' => 'ISBN'), 
+        'ISSN'                => array('dc' => 'Identifier',  'z' => 'ISSN'), 
+        'abstractNote'        => array('dc' => 'Description', 'z' => 'Abstract Note'), 
+        'accessDate'          => array('dc' => 'Date',        'z' => 'Access Date'), 
+        'applicationNumber'   => array('dc' => 'Identifier',  'z' => 'Application Number'), 
+        'archiveLocation'     => array('dc' => 'Publisher',   'z' => 'Archive Location'), 
+        'artworkMedium'       => array('dc' => 'Format',      'z' => 'Artwork Medium'), 
+        'artworkSize'         => array('dc' => 'Format',      'z' => 'Artwork Size'), 
+        'assignee'            => array('dc' => 'Contributor', 'z' => 'Assignee'), 
+        'audioFileType'       => array('dc' => 'Type',        'z' => 'Audio File Type'), 
+        'audioRecordingType'  => array('dc' => 'Type',        'z' => 'Audio Recording Type'), 
+        'billNumber'          => array('dc' => 'Identifier',  'z' => 'Bill Number'), 
+        'blogTitle'           => array('dc' => 'Source',      'z' => 'Blog Title'), 
+        'bookTitle'           => array('dc' => 'Source',      'z' => 'Book Title'), 
+        'callNumber'          => array('dc' => 'Identifier',  'z' => 'Call Number'), 
+        'caseName'            => array('dc' => 'Title',       'z' => 'Case Name'), 
+        'code'                => array('dc' => 'Identifier',  'z' => 'Code'), 
+        'codeNumber'          => array('dc' => 'Identifier',  'z' => 'Code Number'), 
+        'codePages'           => array('dc' => 'Source',      'z' => 'Code Pages'), 
+        'codeVolume'          => array('dc' => 'Source',      'z' => 'Code Volume'), 
+        'committee'           => array('dc' => 'Creator',     'z' => 'Committee'), 
+        'company'             => array('dc' => 'Creator',     'z' => 'Company'), 
+        'conferenceName'      => array('dc' => 'Source',      'z' => 'Conference Name'), 
+        'court'               => array('dc' => 'Creator',     'z' => 'Court'), 
+        'date'                => array('dc' => 'Date',        'z' => 'Date'), 
+        'dateDecided'         => array('dc' => 'Date',        'z' => 'Date Decided'), 
+        'dateEnacted'         => array('dc' => 'Date',        'z' => 'Date Enacted'), 
+        'dictionaryTitle'     => array('dc' => 'Source',      'z' => 'Dictionary Title'), 
+        'distributor'         => array('dc' => 'Publisher',   'z' => 'Distributor'), 
+        'docketNumber'        => array('dc' => 'Identifier',  'z' => 'Docket Number'), 
+        'documentNumber'      => array('dc' => 'Identifier',  'z' => 'Document Number'), 
+        'edition'             => array('dc' => 'Source',      'z' => 'Edition'), 
+        'encyclopediaTitle'   => array('dc' => 'Source',      'z' => 'Encyclopedia Title'), 
+        'episodeNumber'       => array('dc' => 'Source',      'z' => 'Episode Number'), 
+        'extra'               => array('dc' => 'Description', 'z' => 'Extra'), 
+        'firstPage'           => array('dc' => 'Source',      'z' => 'First Page'), 
+        'forumTitle'          => array('dc' => 'Source',      'z' => 'Forum Title'), 
+        'history'             => array('dc' => 'Description', 'z' => 'History'), 
+        'institution'         => array('dc' => 'Publisher',   'z' => 'Institution'), 
+        'interviewMedium'     => array('dc' => 'Format',      'z' => 'Interview Medium'), 
+        'issue'               => array('dc' => 'Source',      'z' => 'Issue'), 
+        'issueDate'           => array('dc' => 'Date',        'z' => 'Issue Date'), 
+        'itemType'            => array('dc' => 'Type',        'z' => 'Item Type'), 
+        'journalAbbreviation' => array('dc' => 'Publisher',   'z' => 'Journal Abbreviation'), 
+        'label'               => array('dc' => 'Publisher',   'z' => 'Label'), 
+        'language'            => array('dc' => 'Language',    'z' => 'Language'), 
+        'legalStatus'         => array('dc' => 'Description', 'z' => 'Legal Status'), 
+        'legislativeBody'     => array('dc' => 'Creator',     'z' => 'Legislative Body'), 
+        'letterType'          => array('dc' => 'Type',        'z' => 'Letter Type'), 
+        'manuscriptType'      => array('dc' => 'Type',        'z' => 'Manuscript Type'), 
+        'mapType'             => array('dc' => 'Type',        'z' => 'Map Type'), 
+        'medium'              => array('dc' => 'Format',      'z' => 'Medium'), 
+        'meetingName'         => array('dc' => 'Source',      'z' => 'Meeting Name'), 
+        'nameOfAct'           => array('dc' => 'Title',       'z' => 'Name Of Act'), 
+        'network'             => array('dc' => 'Publisher',   'z' => 'Network'), 
+        'numPages'            => array('dc' => 'Format',      'z' => 'Num Pages'), 
+        'numberOfVolumes'     => array('dc' => 'Source',      'z' => 'Number Of Volumes'), 
+        'pages'               => array('dc' => 'Source',      'z' => 'Pages'), 
+        'patentNumber'        => array('dc' => 'Identifier',  'z' => 'Patent Number'), 
+        'place'               => array('dc' => 'Publisher',   'z' => 'Place'), 
+        'postType'            => array('dc' => 'Type',        'z' => 'Post Type'), 
+        'presentationType'    => array('dc' => 'Type',        'z' => 'Presentation Type'), 
+        'priorityNumbers'     => array('dc' => 'Identifier',  'z' => 'Priority Numbers'), 
+        'proceedingsTitle'    => array('dc' => 'Source',      'z' => 'Proceedings Title'), 
+        'programmingLanguage' => array('dc' => 'Format',      'z' => 'Programming Language'), 
+        'publicLawNumber'     => array('dc' => 'Identifier',  'z' => 'Public Law Number'), 
+        'publicationTitle'    => array('dc' => 'Source',      'z' => 'Publication Title'), 
+        'publisher'           => array('dc' => 'Publisher',   'z' => 'Publisher'), 
+        'references'          => array('dc' => 'Relation',    'z' => 'References'), 
+        'reportNumber'        => array('dc' => 'Identifier',  'z' => 'Report Number'), 
+        'reportType'          => array('dc' => 'Type',        'z' => 'Report Type'), 
+        'reporter'            => array('dc' => 'Contributor', 'z' => 'Reporter'), 
+        'reporterVolume'      => array('dc' => 'Source',      'z' => 'Reporter Volume'), 
+        'repository'          => array('dc' => 'Publisher',   'z' => 'Repository'), 
+        'rights'              => array('dc' => 'Rights',      'z' => 'Rights'), 
+        'runningTime'         => array('dc' => 'Format',      'z' => 'Running Time'), 
+        'scale'               => array('dc' => 'Format',      'z' => 'Scale'), 
+        'section'             => array('dc' => 'Source',      'z' => 'Section'), 
+        'series'              => array('dc' => 'Source',      'z' => 'Series'), 
+        'seriesNumber'        => array('dc' => 'Source',      'z' => 'Series Number'), 
+        'seriesText'          => array('dc' => 'Description', 'z' => 'Series Text'), 
+        'seriesTitle'         => array('dc' => 'Source',      'z' => 'Series Title'), 
+        'session'             => array('dc' => 'Creator',     'z' => 'Session'), 
+        'shortTitle'          => array('dc' => 'Title',       'z' => 'Short Title'), 
+        'studio'              => array('dc' => 'Publisher',   'z' => 'Studio'), 
+        'subject'             => array('dc' => 'Title',       'z' => 'Subject'), 
+        'system'              => array('dc' => 'Publisher',   'z' => 'System'), 
+        'thesisType'          => array('dc' => 'Type',        'z' => 'Thesis Type'), 
+        'title'               => array('dc' => 'Title',       'z' => 'Title'), 
+        'type'                => array('dc' => 'Type',        'z' => 'Type'), 
+        'university'          => array('dc' => 'Publisher',   'z' => 'University'), 
+        'url'                 => array('dc' => 'Identifier',  'z' => 'URL'), 
+        'version'             => array('dc' => 'Relation',    'z' => 'Version'), 
+        'videoRecordingType'  => array('dc' => 'Type',        'z' => 'Video Recording Type'), 
+        'volume'              => array('dc' => 'Source',      'z' => 'Volume'), 
+        'websiteTitle'        => array('dc' => 'Source',      'z' => 'Website Title'), 
+        'websiteType'         => array('dc' => 'Type',        'z' => 'Website Type'), 
+    );
+    
     public static function install()
-    {}
+    {
+        // Insert the Zotero element set.
+        $elementSetMetadata = 'Zotero';
+        $elements = array();
+        foreach (self::$zoteroFields as $zoteroFieldName => $map) {
+            if ('creator' == $zoteroFieldName) {
+                foreach ($map['z'] as $creatorFieldName) {
+                    $elements[] = array('name' => $creatorFieldName, 'data_type' => 'Tiny Text');
+                }
+            } else {
+                $elements[] = array('name' => $map['z'], 'data_type' => 'Tiny Text');
+            }
+        }
+        // Add the Note element, which is separate from the separate fields.
+        $elements[] = array('name' => 'Note', 'data_type' => 'Tiny Text');
+        insert_element_set($elementSetMetadata, $elements);
+    }
     
     public static function uninstall()
     {}
