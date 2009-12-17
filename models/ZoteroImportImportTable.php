658884979f6f94eccc5e8604c2668ffe5ffe47c8
Jim Safley <jimsafley@gmail.com>
2009-12-17 21:13:02 +0000

    Removes the log from the process query, since logging was removed in migration 46.

--- models/ZoteroImportImportTable.php
+++ models/ZoteroImportImportTable.php
@@ -7,7 +7,7 @@ class ZoteroImportImportTable extends Omeka_Db_Table
         $db = $this->getDb();
         
         $select->join(array('c' => $db->Collection), 'c.id = z.collection_id', array('name'))
-               ->join(array('p' => $db->Process), 'p.id = z.process_id', array('pid', 'status', 'started', 'stopped', 'log'));
+               ->join(array('p' => $db->Process), 'p.id = z.process_id', array('pid', 'status', 'started', 'stopped'));
         
         return $this->fetchObjects($select);
     }
