d2acf73c0530e6550c99e158072ed72bf605e28f
Jim Safley <jimsafley@gmail.com>
2010-03-24 20:01:01 +0000

    Adds $limit parameter and cleans up code.

--- helpers/ZoteroImportFunctions.php
+++ helpers/ZoteroImportFunctions.php
@@ -1,22 +1,27 @@
 <?php
-
 /**
- * Returns items of a particular Zotero item type. Uses the Item Type
- * field in the Zotero element set.
+ * Returns items of a particular Zotero item type. Uses the Item Type element in 
+ * the Zotero element set.
  *
  * @param string $typeName
  * @param int $collectionId 
  * @return void
  */
-function get_items_by_zotero_item_type($typeName, $collectionId = null)
+function get_items_by_zotero_item_type($typeName, $collectionId = null, $limit = 10)
 {
-   
-    // Get the table element
     $db = get_db();
+    
+    // Get the Zotero:Item Type element
     $element = $db->getTable('Element')->findByElementSetNameAndElementName('Zotero', 'Item Type');
     
-    // Get the items th
-    $items = get_items(array('collection' => $collectionId, 'recent' => true, 'advanced_search' => array(array('type' => 'contains', 'element_id' => $element->id, 'terms' => $typeName))));
+    // Using the advanced search interface, get a limited set of items that have 
+    // the provided Zotero:Item Type.
+    $items = get_items(array('collection' => $collectionId, 
+                             'recent' => true, 
+                             'advanced_search' => array(array('type' => 'contains', 
+                                                              'element_id' => $element->id, 
+                                                              'terms' => $typeName))), 
+                       $limit);
     
     return $items;
 }
